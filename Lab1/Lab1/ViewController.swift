0e258bc442ffef7912a031b22625926ca3bbe340
Sean <shawn.casaus@gmail.com>
2018-08-31 12:02:43 -0700

    connected components to viewcontroller

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var bmiField: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateBMI(_ sender: Button) {
+        print("hello world")
+    }
+    
 }
 
