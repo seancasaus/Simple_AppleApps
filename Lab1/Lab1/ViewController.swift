db93f8ab925b5e92e37384cd0c5365e6b03ebece
Sean <shawn.casaus@gmail.com>
2018-08-31 22:21:16 -0700

    Perfomed calculation for slider

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //View 1 components
     @IBOutlet weak var heightField: UITextField!
     @IBOutlet weak var weightField: UITextField!
     @IBOutlet weak var bmiField: UILabel!
     @IBOutlet weak var classificationField: UILabel!
     
+    //slider view components
+    @IBOutlet weak var heightSliderLabel: UILabel!
+    @IBOutlet weak var weightSliderLabel: UILabel!
+    @IBOutlet weak var bmiSliderLabel: UILabel!
+    
+    struct sliderVariables {
+        static var height:Int = 0
+        static var weight:Int = 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +35,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //button click
     @IBAction func calculateBMI(_ sender: Button) {
         
         //height and weight must both be doubles
@@ -36,7 +48,7 @@ class ViewController: UIViewController {
                 //set BMI text field
                 bmiField.text = String.localizedStringWithFormat("%.2f %@", BMI, "")
                 
-                //out put message
+                //out put messages, change color
                 if BMI < 18 {
                     classificationField.textColor = UIColor.blue
                     classificationField.text = "You are underweight"
@@ -60,5 +72,38 @@ class ViewController: UIViewController {
         }
     }
     
+    func sliderController() {
+        //print("\(sliderVariables.height) | \(sliderVariables.weight)")
+        let height:Int = sliderVariables.height
+        let weight:Int = sliderVariables.weight
+        
+        
+        heightSliderLabel.text = String(height)
+        weightSliderLabel.text = String(weight)
+        
+        if height != 0 && weight != 0 {
+            let BMI:Double = (Double(weight) / Double(height) / Double(height)) * 703
+            
+            if BMI <= 50 && BMI >= 8 {
+                bmiSliderLabel.text = String.localizedStringWithFormat("%.2f %@", BMI, "")
+            }
+            else {
+                bmiSliderLabel.text = "###"
+            }
+        }
+    }
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        sliderVariables.height = Int(sender.value)
+        sliderController()
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        sliderVariables.weight = Int(sender.value)
+        sliderController()
+    }
+    
+    
 }
+
 
