19464be9aad62b6645351a1e360a2286e6e17063
Sean <shawn.casaus@gmail.com>
2018-10-21 20:00:43 -0700

    Created City View

--- TableView/TableView/CityController.swift
+++ TableView/TableView/CityController.swift
@@ -9,27 +9,14 @@
 import UIKit
 
 class CityController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descView: UILabel!
+    @IBOutlet weak var titleView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
