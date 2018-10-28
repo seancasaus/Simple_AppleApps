daa38519eb3b03eeb7394a5b8ebd93936ab76013
Sean <shawn.casaus@gmail.com>
2018-10-28 14:06:54 -0700

    Clicking a city now grabs the coordinates for that city

--- TableView/TableView/CityController.swift
+++ TableView/TableView/CityController.swift
@@ -19,11 +19,41 @@ class CityController: UIViewController {
     var i:Int?
     var citys:Model?
     
+    var lon : CLLocationDegrees = 0
+    var lat : CLLocationDegrees = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         titleView.text = citys?.cityArray[i!].title
         descView.text = citys?.cityArray[i!].description
         imageView.image = citys?.cityArray[i!].image
+        let addressString = citys?.cityArray[i!].title
+        
+        CLGeocoder().geocodeAddressString(addressString!, completionHandler:
+        {(placemarks, error) in
+                
+            if error != nil {
+                print("Geocode failed: \(error!.localizedDescription)")
+            } else if placemarks!.count > 0 {
+                let placemark = placemarks![0]
+                let location = placemark.location
+                let coords = location!.coordinate
+                self.lon = coords.longitude
+                self.lat = coords.latitude
+                print(location!)
+                
+                let span = MKCoordinateSpanMake(0.05, 0.05)
+                let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                self.map.setRegion(region, animated: true)
+                let ani = MKPointAnnotation()
+                ani.coordinate = placemark.location!.coordinate
+                ani.title = placemark.locality
+                ani.subtitle = placemark.subLocality
+                
+                self.map.addAnnotation(ani)
+            }
+        })
+
         
         // Do any additional setup after loading the view.
     }
@@ -44,23 +74,24 @@ class CityController: UIViewController {
         }
         
         // display the map
-        let lon : CLLocationDegrees = -112.06
         
-        let lat : CLLocationDegrees = 33.45
-        
+        if lon == 0 || lat == 0 {
+            self.lon = -112.06
+            self.lat = 33.45
+        }
+
         let coordinates = CLLocationCoordinate2D( latitude: lat, longitude: lon)
         let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        
+
         let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinates, span)
-        
+
         self.map.setRegion(region, animated: true)
-        
+
         // add an annotation
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinates
-        annotation.title = "Phoenix"
-        annotation.subtitle = "AZ"
-        
+        annotation.title = titleView.text
+
         self.map.addAnnotation(annotation)
         
     }
