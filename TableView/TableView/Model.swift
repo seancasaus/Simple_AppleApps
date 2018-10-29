3c856392c14fc31be34fa998f4987169c8bd4a8e
Sean <shawn.casaus@gmail.com>
2018-10-28 20:42:23 -0700

    fixed view issue with photos

--- TableView/TableView/Model.swift
+++ TableView/TableView/Model.swift
@@ -13,11 +13,11 @@ class Model {
     var cityArray:[City] = []
     
     init() {
-        addCity(name: "Paris", desc: "Paris, the largest city and capital of France, was founded in 52 BC and is known as the City of Lights.", image: #imageLiteral(resourceName: "paris"))
-        addCity(name: "Austin", desc: "Austin, the capital of Texas, was founded in 1835 and is known as the Live Music Capital of the world.", image: #imageLiteral(resourceName: "austin"))
-        addCity(name: "San Diego", desc: "San Diego, the 3rd largest metropolitan area in California, was founded in 1769 and is known for its' laid back vibes and surf culture.", image: #imageLiteral(resourceName: "sanDiego"))
-        addCity(name: "Tampa", desc: "Tampa, the second largest metropolitan area in Florida, was founded in 1849 and is known for its' Cuban heritage and importance in the Cigar industry.", image: #imageLiteral(resourceName: "tampa"))
-        addCity(name: "Seattle", desc: "Seattle, the largest city in the Pacific North West, was founded in 1869 and is known as the Emerald City.", image: #imageLiteral(resourceName: "seattle"))
+        addCity(name: "Paris, France", desc: "Paris, the largest city and capital of France, was founded in 52 BC and is known as the City of Lights.", image: #imageLiteral(resourceName: "paris"))
+        addCity(name: "Austin, Texas", desc: "Austin, the capital of Texas, was founded in 1835 and is known as the Live Music Capital of the world.", image: #imageLiteral(resourceName: "austin"))
+        addCity(name: "San Diego, California", desc: "San Diego, the 3rd largest metropolitan area in California, was founded in 1769 and is known for its' laid back vibes and surf culture.", image: #imageLiteral(resourceName: "sanDiego"))
+        addCity(name: "Tampa, Florida", desc: "Tampa, the second largest metropolitan area in Florida, was founded in 1849 and is known for its' Cuban heritage and importance in the Cigar industry.", image: #imageLiteral(resourceName: "tampa"))
+        addCity(name: "Seattle, Washington", desc: "Seattle, the largest city in the Pacific North West, was founded in 1869 and is known as the Emerald City.", image: #imageLiteral(resourceName: "seattle"))
     }
     
     func addCity(name: String, desc: String, image: UIImage) {
