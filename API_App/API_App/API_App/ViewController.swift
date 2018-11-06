70cd74e1972a6c111f051eff499205173489d1cb
Sean <shawn.casaus@gmail.com>
2018-11-06 11:24:53 -0700

    used mapkit to get coordinaes for entered location

--- API_App/API_App/API_App/ViewController.swift
+++ API_App/API_App/API_App/ViewController.swift
@@ -7,13 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityName: UITextField!
+    var lon : CLLocationDegrees = 0
+    var lat : CLLocationDegrees = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func getInfo(_ sender: Any) {
+        let cityName = self.cityName.text
+        CLGeocoder().geocodeAddressString(cityName!, completionHandler:
+            {(placemarks, error) in
+                if error != nil {
+                    print("Geocode failed: \(error!.localizedDescription)")
+                } else {
+                    let placemark = placemarks![0]
+                    let location = placemark.location
+                    let coords = location!.coordinate
+                    self.lon = coords.longitude
+                    self.lat = coords.latitude
+                    //print("Long: \(self.lon) | Lat: \(self.lat)")
+                    let north = self.lat + 10
+                    let south = self.lat - 10
+                    let east = self.lon - 10
+                    let west = self.lon + 10
+                    let urlAsString = "http://api.geonames.org/earthquakesJSON?north="+String(format:"%f", north)+"&south="+String(format:"%f", south)+"&east="+String(format:"%f", east)+"&west="+String(format:"%f", west)+"&username=demo"
+                    print("\(north) \(south) \(east) \(west)");
+                    print(urlAsString);
+                }
+        })
+        
+    }
+    
+    
+    
+    
 
 
 }
